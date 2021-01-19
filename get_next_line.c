d5499df2aaf30e3a37132047b841ef4e7816b261
zjamali <zouhairjamali1998@gmail.com>
2021-01-19 13:00:39 +0100

    working in get_

--- get_next_line.c
+++ get_next_line.c
@@ -9,11 +9,46 @@ typedef struct s_buff
 	size_t	pos;
 }				t_buff;
 
+void copy(t_buff *dest,t_buff *src)
+{
+	/// copy content of prev to current 
+	dest->buff = src->buff;
+	dest->pos = src->pos;
+	dest->size = src->size;
+
+	/// set previous to NULL
+	src->buff = NULL;
+	src->size = NULL;
+	src->pos = NULL;
+}
+int check(char *str,size_t len)
+{
+	int i;
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '\n')
+			return i;
+		i++;
+	}
+	return -1; 
+}
 int get_next_line(char **line)
 {
 	static t_buff	prev;
 	t_buff			curr;
 
+	if (prev.buff != NULL)
+	{
+		copy(&curr,&prev);
 
+	}
+	else
+	{
+
+		
+	}
+	
+	
 
 }
